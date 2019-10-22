54f1cce3b2f2a25bfecb4e6f6129cd5c79475269
KsushaRomano <55737387+KsushaRomano@users.noreply.github.com>
2019-10-22 15:19:22 +0300

    Add files via upload

---
+++ Laba_22_2.cpp
@@ -0,0 +1,28 @@
+﻿using namespace std;
+
+#include <iostream>
+#include <string>
+#include <windows.h>
+#include <fstream>
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	int i, j, m, N, K;
+	string S, Filename;
+	fstream F;
+	cout << "Введите имя файла: ";
+	getline(cin, Filename);
+	cout << "Введите количество строк и столбцов: ";
+	cin >> N >> K;
+	F.open(Filename, ios::out);
+	for (i = 0; i < N; i++) {
+		for (j = 0; j < K; j++) {
+			F << '*';
+		};
+		F << endl;
+	};
+	F.close();
+}
